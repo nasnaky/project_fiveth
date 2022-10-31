e7219469698baf8c8fd4f0f8c0d72bbef2c4948a
정재훈 <81303380+nasnaky@users.noreply.github.com>
2022-10-31 20:56:59 +0900

    urls,seializer,view(S)
    url,seializer,view 의 사전 구성

---
+++ editorialserver/Dserver/serializer.py
@@ -0,0 +1,2 @@
+from  rest_framework import serializers
+from .models import CAMERA,TEMPERATURE,HUMIDITY,REMNANT_W
--- editorialserver/Dserver/views.py
+++ editorialserver/Dserver/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from django.db.models import Q
+from datetime import date, datetime
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+# @api_view(['GET'])
+# def steatus_return(request):
+#     if request.method == "GET":
+#         time = datetime.now()
