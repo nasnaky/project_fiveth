e4c6e1ca00b88bbb42d97ba885423e340b3350cf
정재훈 <81303380+nasnaky@users.noreply.github.com>
2022-10-31 20:01:36 +0900

    Model(S)
    사설서버 초기 모델링

--- editorialserver/Dserver/models.py
+++ editorialserver/Dserver/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class TEMPERATURE(models.Model):  # 온도
+    TEMP = models.IntegerField()
+    TIME = models.DateTimeField(auto_now_add=True)
+
+
+class HUMIDITY(models.Model):  # 습도
+    DEGREE = models.IntegerField()
+    TIME = models.DateTimeField(auto_now_add=True)
+
+
+class REMNANT_W(models.Model):
+    WATER = models.IntegerField()
+    TIME = models.DateTimeField(auto_now_add=True)
+
+
+class CAMERA(models.Model):
+    TITLE = models.TextField()
+    LINK = models.URLField()
+    
