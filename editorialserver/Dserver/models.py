6dcd2b2d46c72715e3654b1259310d635195e631
정재훈 <81303380+nasnaky@users.noreply.github.com>
2022-11-01 09:40:36 +0900

    data
    더미데이터 저장

--- editorialserver/Dserver/models.py
+++ editorialserver/Dserver/models.py
@@ -3,17 +3,17 @@ from django.db import models
 
 class TEMPERATURE(models.Model):  # 온도
     TEMP = models.IntegerField()
-    TIME = models.DateTimeField(auto_now_add=True)
+    TIME = models.DateTimeField()
 
 
 class HUMIDITY(models.Model):  # 습도
     DEGREE = models.IntegerField()
-    TIME = models.DateTimeField(auto_now_add=True)
+    TIME = models.DateTimeField()
 
 
 class REMNANT_W(models.Model):
     WATER = models.IntegerField()
-    TIME = models.DateTimeField(auto_now_add=True)
+    TIME = models.DateTimeField()
 
 
 class CAMERA(models.Model):
