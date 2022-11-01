3159111bf72d5ad657be12067daa4f22a97a92c7
정재훈 <81303380+nasnaky@users.noreply.github.com>
2022-11-01 20:45:58 +0900

    model  DB
    아두이노 용 서버
    model 설정 변경

--- Arduinoserver/Dserver/models.py
+++ Arduinoserver/Dserver/models.py
@@ -3,17 +3,17 @@ from django.db import models
 
 class TEMPERATURE(models.Model):  # 온도
     TEMP = models.IntegerField()
-    TIME = models.DateTimeField()
+    TIME = models.DateTimeField(auto_now_add=True)
 
 
 class HUMIDITY(models.Model):  # 습도
     DEGREE = models.IntegerField()
-    TIME = models.DateTimeField()
+    TIME = models.DateTimeField(auto_now_add=True)
 
 
 class REMNANT_W(models.Model):
     WATER = models.IntegerField()
-    TIME = models.DateTimeField()
+    TIME = models.DateTimeField(auto_now_add=True)
 
 
 class CAMERA(models.Model):
