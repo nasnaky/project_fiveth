7b476e75f68634a3d5fec3a541d3a67879f947b5
정재훈 <81303380+nasnaky@users.noreply.github.com>
2022-11-02 10:16:11 +0900

    urls (M)
    url 사전 구성

--- Arduinoserver/Dserver/views.py
+++ Arduinoserver/Dserver/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-from rest_framework import status, viewsets
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
-from django.db.models import Q, Avg
-from datetime import date, datetime, timedelta
+from django.db.models import Avg
+from datetime import timedelta
 from rest_framework.decorators import api_view
 from django.utils import timezone
 
--- Mserver/MFID/views.py
+++ Mserver/MFID/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from django.db.models import Q, Avg
+from datetime import date, datetime, timedelta
+from rest_framework.decorators import api_view
+from django.utils import timezone
