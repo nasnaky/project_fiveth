9aa5829059b219d2d0e6f1ea251a616fb870be55
정재훈 <81303380+nasnaky@users.noreply.github.com>
2022-11-02 13:06:50 +0900

    model(S)
    메인서버 모델링

--- Mserver/MFID/models.py
+++ Mserver/MFID/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 
+class USER(models.Model):
+    NAME = models.TextField()
+    PASSWORD = models.TextField()
+
+
+class SERVER(models.Model):
+    LINK = models.URLField()
+    TITLE = models.TextField()
+    CENTENT = models.TextField()
+    STATUS = models.IntegerField(default=0)
+    USER = models.ForeignKey(USER, on_delete=models.CASCADE)
