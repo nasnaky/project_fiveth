bec50c08ae6a33d0653e90ab7d4c5affc1aa813f
정재훈 <81303380+nasnaky@users.noreply.github.com>
2022-11-07 18:19:58 +0900

    views
    정렬
    '

--- Mserver/MFID/views.py
+++ Mserver/MFID/views.py
@@ -1,16 +1,9 @@
-from django.shortcuts import render
-from rest_framework import status, viewsets
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
-from django.db.models import Q, Avg
-from datetime import date, datetime, timedelta
 from rest_framework.decorators import api_view
-from django.utils import timezone
 import requests
 
 from .models import USER, SERVER
 from .serializer import USERSerializers, SERVERSerializers, SERVERCreateSerializers, SERVERListSerializers
-
 
 
 @api_view(['POST'])
